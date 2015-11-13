b3d9f5ba233bf97254b9185a148b550f1b32f1e8
George <osadze@hotmail.com>
2015-11-12 21:59:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+#Setup
+import pygame
+from pygame.locals import *
+from startscreen import *
+pygame.init()
+window=pygame.display.set_mode([1300,640],0,24)
+pygame.display.set_caption("Space Py Continuum")
+clock=pygame.time.Clock()
+
+#Display welcome screen
+StartScreen(window)
