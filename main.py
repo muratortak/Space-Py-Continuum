6df3e5afa333dd9dfbf6986e7fc9ad63537d37c8
George <osadze@hotmail.com>
2015-11-15 01:46:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-
-#Setup
 import pygame
 from pygame.locals import *
-from startscreen import *
-pygame.init()
-window=pygame.display.set_mode([1300,640],0,24)
-pygame.display.set_caption("Space Py Continuum")
-clock=pygame.time.Clock()
-
-#Display welcome screen
-StartScreen(window)
+def SPC_events():
+	for event in pygame.event.get():
+		   #quit event
+     	   if event.type == QUIT:
+        	    exit()
+           #keyboard events
+           if event.type == pygame.KEYDOWN:
+               #move keys
+               if event.key == pygame.K_a:
+                   print('left')
+               if event.key == pygame.K_d:
+                   print('right')
+               if event.key == pygame.K_w:
+                   print('forward')
+               if event.key == pygame.K_s:
+                   print('backwards')
+                   
+               #ETC
+               if event.key == pygame.K_SPACE:
+                   print('space')
+               if event.key == pygame.K_b:
+                   print('build')
+  	    
