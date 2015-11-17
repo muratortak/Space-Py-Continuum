19c6cb37d021c0c9b335efd55d2021f3e7e003bd
Kilimo <crforce402@gmail.com>
2015-11-16 21:42:18 -0500

    Testing

--- startscreen.py
+++ startscreen.py
@@ -3,5 +3,6 @@ from pygame.locals import *
 def SPC_startscreen(window):
 	font=pygame.font.SysFont('arial',120)
 	window.fill((0,0,0))
+#This is a test
 	window.blit(font.render("Space Py Continuum",True,(0,255,0)),(100,200))
 	pygame.display.flip()
