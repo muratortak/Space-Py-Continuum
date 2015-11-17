aacdc997dc50b1bb7cd9dbe86194960bd9e01881
fowel <osadze@hotmail.com>
2015-11-16 22:10:39 -0500

    added global vars to events file

--- events.py
+++ events.py
@@ -1,6 +1,10 @@
 import pygame
+from globals import *
 from pygame.locals import *
 def SPC_events():
+        global gy
+        global gx
+        global gz
 	for event in pygame.event.get():
 		   #quit event
      	   if event.type == QUIT:
@@ -9,17 +13,22 @@ def SPC_events():
            if event.type == pygame.KEYDOWN:
                #move keys
                if event.key == pygame.K_a:
-                   print('left')
+                   gx = gx-1
+                   print gx
                if event.key == pygame.K_d:
-                   print('right')
+                   gx = gx+1
+                   print gx
                if event.key == pygame.K_w:
-                   print('forward')
+                   gy = gy+1
+                   print gy
                if event.key == pygame.K_s:
-                   print('backwards')
-                   
+                   gy = gy-1
+                   print gy
                #ETC
                if event.key == pygame.K_SPACE:
                    print('space')
                if event.key == pygame.K_b:
                    print('build')
+               if event.key == pygame.K_g:
+                   print('key g')
   	    
